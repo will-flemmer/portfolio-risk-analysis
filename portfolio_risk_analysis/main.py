2baea83e13303401ca638af85341e602666a4c8a
William Flemmer <flemmerwill@gmail.com>
2024-04-11 12:16:09 +0100

    calculate monthly portfolio volatility

--- portfolio_risk_analysis/main.py
+++ portfolio_risk_analysis/main.py
@@ -1,18 +1,26 @@
 from portfolio import Portfolio
+import matplotlib.pyplot as plt
 
 all_apple = { "AAPL": 100 }
 mixed = {
-  "AAPL": 35,
-  "MSFT": 20,
+  "AAPL": 25,
+  "MSFT": 5,
   "TSLA": 5,
   "C": 5,
-  "GS": 5,
+  "GS": 30,
   "SPCE": 30
 }
 volatile = {
   "VINC": 50,
-  "MGAM": 50
+  "MGAM": 50,
 }
-portfolio = Portfolio(volatile, "1y")
+portfolio = Portfolio(mixed, "5y")
 
-portfolio.calculate_portfolio_volatility()
+portfolio_volatility = portfolio.calculate_portfolio_volatility() * 100
+print(f"Portfolio volatility: {portfolio_volatility}%")
+
+monthly_volatility = portfolio.calculate_portfolio_volatility_against_time() * 100
+monthly_volatility.plot(title="Monthly Volatility Vs Time")
+plt.xlabel('Date')
+plt.ylabel('Volatility(%)')
+plt.show()
--- portfolio_risk_analysis/portfolio.py
+++ portfolio_risk_analysis/portfolio.py
@@ -1,5 +1,4 @@
 import yfinance as yf
-import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
@@ -9,8 +8,10 @@ class PortfolioValidationError(Exception):
 class Portfolio():
   def __init__(self, stock_portions, time_period):
     self.stock_portions = stock_portions
-    self.time_period = time_period
     self.validate()
+    self.time_period = time_period
+    self.weights = np.array([x / 100 for x in self.stock_portions.values()])
+    self.create_portfolio_df()
 
   def validate(self):
     if not bool(self.stock_portions):
@@ -18,22 +19,43 @@ class Portfolio():
     total_percentage_alloc = sum(self.stock_portions.values())
     if total_percentage_alloc != 100:
       raise PortfolioValidationError(f"Portfolio allocation does not sum to 100%, it sums to {total_percentage_alloc}%")
-
-  def calculate_portfolio_volatility(self):
-    cov_matrix = self.create_covariance_matrix()
-    weights = np.array([x / 100 for x in self.stock_portions.values()])
-    portfolio_volatility = np.sqrt(np.dot(weights, np.dot(cov_matrix, weights)))
-    print(f"Portfolio volatility: {portfolio_volatility * 100}%")
-
-  def create_covariance_matrix(self):
+  
+  def create_portfolio_df(self):
     close_prices = {}
     for stock_symbol in self.stock_portions.keys():
       close_prices[stock_symbol] = yf.Ticker(stock_symbol).history(period=self.time_period)['Close']
 
-    stock_df = pd.DataFrame(close_prices)
-    stock_df = stock_df.pct_change().dropna()
+    self.portfolio_df = pd.DataFrame(close_prices)
 
-    # This line ensures there are no NaN values in stock_df
-    assert(stock_df[stock_df.isna().any(axis=1)].empty) == True
+  def calculate_portfolio_volatility_against_time(self):
+    window_size_days = 30
+    returns_df = self.daily_returns_df()
+    volatility_df = returns_df.rolling(window_size_days).std().dropna()
 
-    return stock_df.cov()
+    for end in range(window_size_days, len(returns_df) + 1):
+      monthly_returns_df = returns_df[end - window_size_days: end]
+      date_index = monthly_returns_df.tail(1).index
+      cov_matrix = self.create_covariance_matrix(monthly_returns_df)
+      volatility = self.calculate_volatility(cov_matrix)
+      volatility_df.loc[date_index, "Portfolio"] = volatility
+
+    self.check_df_has_no_nan_values(returns_df)
+    return volatility_df
+
+  def calculate_portfolio_volatility(self):
+    cov_matrix = self.create_covariance_matrix(self.daily_returns_df())
+    return self.calculate_volatility(cov_matrix)
+  
+  def calculate_volatility(self, cov_matrix):
+    portfolio_variance = np.dot(self.weights, np.dot(cov_matrix, self.weights))
+    return np.sqrt(portfolio_variance)
+
+  def create_covariance_matrix(self, returns_df):
+    self.check_df_has_no_nan_values(returns_df)
+    return returns_df.cov()
+  
+  def daily_returns_df(self):
+    return self.portfolio_df.pct_change().dropna()
+  
+  def check_df_has_no_nan_values(self, df):
+    assert(df[df.isna().any(axis=1)].empty) == True
