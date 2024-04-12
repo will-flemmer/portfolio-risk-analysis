e90858c672deda82f4ad6f73248d17aa50c5d863
William Flemmer <flemmerwill@gmail.com>
2024-04-12 10:56:44 +0100

    test volatility calculation

--- tests/test_portfolio.py
+++ tests/test_portfolio.py
@@ -9,10 +9,17 @@ def create_mock_stock_data(days):
   start_date = datetime.now() - timedelta(days=days)
   end_date = datetime.now()
   date_list = [start_date + timedelta(days=x) for x in range((end_date - start_date).days + 1)]
+
   vals = np.ones(len(date_list))
+  
   df = pd.DataFrame({"Close": vals, "Date": date_list})
   df.set_index(["Date"], inplace=True)
   return df
+
+def mock_yfinance(mocked_ticker, num_days):
+  mocked_instance = MagicMock()
+  mocked_instance.history.return_value = create_mock_stock_data(num_days)
+  mocked_ticker.return_value = mocked_instance
 
 
 class TestPortfolioInit:
@@ -32,25 +39,46 @@ class TestPortfolioInit:
 
   @patch("yfinance.Ticker")
   def test_portfolio_weights_are_stored_correctly(self, mocked_ticker):
-    mocked_instance = MagicMock()
-    mocked_instance.history.return_value = create_mock_stock_data((365 * 2))
-    mocked_ticker.return_value = mocked_instance
-    p = Portfolio({"AAPL": 80, "MSFT": 20}, "2y")
+    mock_yfinance(mocked_ticker, 365)
+    p = Portfolio({"AAPL": 80, "MSFT": 20}, "1y")
     expected_weights = np.array([0.8, 0.2])
-    assert(np.all(np.equal(p.weights, expected_weights))) == True
+    assert np.all(np.equal(p.weights, expected_weights))
 
 
   @patch("yfinance.Ticker")
   def test_portfolio_df_is_created(self, mocked_ticker):
-    num_days = 365 * 2
-    mocked_instance = MagicMock()
-    mocked_instance.history.return_value = create_mock_stock_data(num_days)
-    mocked_ticker.return_value = mocked_instance
-    p = Portfolio({"AAPL": 80, "MSFT": 20}, "2y")
+    num_days = 365
+    mock_yfinance(mocked_ticker, num_days)
+    p = Portfolio({"AAPL": 80, "MSFT": 20}, "1y")
 
     expected_columns = ["AAPL", "MSFT"]
     assert len(p.portfolio_df.columns) == len(expected_columns)
     assert all([actual == expected for actual, expected in zip(p.portfolio_df.columns, expected_columns)])
 
-    assert(len(p.portfolio_df["AAPL"])) == num_days + 1
-    assert(len(p.portfolio_df["MSFT"])) == num_days + 1
+    assert len(p.portfolio_df["AAPL"]) == num_days + 1
+    assert len(p.portfolio_df["MSFT"]) == num_days + 1
+
+@patch("yfinance.Ticker")
+def test_check_df_has_no_nan_values(mocked_ticker):
+    mock_yfinance(mocked_ticker, 365)
+    p = Portfolio({"AAPL": 100}, "1y")
+
+    p.check_df_has_no_nan_values(p.portfolio_df)
+
+    p.portfolio_df.loc[0, "AAPL"] = np.nan
+    with pytest.raises(AssertionError):
+        p.check_df_has_no_nan_values(p.portfolio_df)
+
+@patch("yfinance.Ticker")
+def test_calculate_volatility(mocked_ticker):
+    mock_yfinance(mocked_ticker, 365)
+    p = Portfolio({"AAPL": 40, "MSFT": 60}, "1y")
+
+    returns_df = pd.DataFrame({"AAPL": [1, 2, 3], "MSFT": [6, 5, 4]})
+    cov_matrix = returns_df.cov()
+    
+    expected_variance = np.dot(p.weights, np.dot(cov_matrix, p.weights))
+    expected_volatility = np.sqrt(expected_variance)
+
+    actual_volatility = p.calculate_volatility(cov_matrix)
+    assert expected_volatility == actual_volatility
